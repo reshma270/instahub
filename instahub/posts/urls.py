fa1da86c7310a0478e4c42db3d4cffab0b73d3e1
Reshma <reshma.to.thomas@gmail.com>
2024-07-06 16:06:27 -0400

    Implement post editing and deletion functionality.

--- instahub/posts/urls.py
+++ instahub/posts/urls.py
@@ -1,8 +1,12 @@
 # posts/urls.py
 
 from django.urls import path
-from .views import create_post
+from .views import create_post, delete_post, edit_post
 
 urlpatterns = [
     path("create/", create_post, name="create_post"),
+    path("edit/<int:post_id>/", edit_post, name="edit_post"),  # URL for editing posts
+    path(
+        "delete/<int:post_id>/", delete_post, name="delete_post"
+    ),  # URL for deleting posts
 ]
--- instahub/posts/views.py
+++ instahub/posts/views.py
@@ -1,8 +1,9 @@
 # posts/views.py
 
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PostCreationForm
+from .models import Post
 
 
 # Create your views here.
@@ -19,3 +20,25 @@ def create_post(request):
     else:
         form = PostCreationForm()
     return render(request, "posts/create_post.html", {"form": form})
+
+
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id, author=request.user)
+    if request.method == "POST":
+        form = PostCreationForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = PostCreationForm(instance=post)
+    return render(request, "posts/edit_post.html", {"form": form})
+
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id, author=request.user)
+    if request.method == "POST":
+        post.delete()
+        return redirect("profile")
+    return render(request, "posts/delete_post.html", {"post": post})
