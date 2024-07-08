55443faa3c09cf58f13cdf3f927f3801cada9d7b
Reshma <reshma.to.thomas@gmail.com>
2024-07-08 18:26:52 -0400

    Configure the post management section in the admin panel.

--- instahub/posts/admin.py
+++ instahub/posts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "caption", "created_at", "total_likes")
+    search_fields = ("author__username", "caption")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+
+    def total_likes(self, obj):
+        return obj.likes.count()
+
+    total_likes.short_description = "Likes"
+
+
+admin.site.register(Post, PostAdmin)
